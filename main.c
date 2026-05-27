12aebc1e96c9ac00247420bd1c75e0b0b0bf18a9
Anis Belkahla <belcalaanis@gmail.com>
2026-05-27 00:30:31 -0400

    parsing (logic needs to be changed)

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <stdlib.h>
 
 void listDirectory(const char *basePath)
 {
@@ -23,16 +24,30 @@ int main()
         if (token != NULL && strcmp(token, "ls") == 0)
         {
             listDirectory(".");
-            printf("first command\n");
         }
         else
         {
-            printf("wrong command\n");
+            printf("couldn't find the command\n");
         }
+        int argument = 0;
+        int size = 1;
+        char **commande = malloc(size * sizeof(commande));
         while (token != NULL)
         {
-            printf("%s\n", token);
+            commande[argument] = token;
             token = strtok(NULL, " ");
+            argument++;
+            if (token != NULL)
+            {
+                size++;
+                commande = realloc(commande, size * sizeof(commande));
+            }
         }
+        for (int i = 0; i < size; i++)
+        {
+            printf("%s\n", commande[i]);
+        }
+        printf("%d", argument);
+        free(commande);
     }
 }
